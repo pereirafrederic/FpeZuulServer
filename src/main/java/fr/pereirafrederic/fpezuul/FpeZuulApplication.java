9be6e2be023a2f48293984a7303a72835bff275d
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-04-04 14:37:31 +0200

    init

---
+++ src/main/java/fr/pereirafrederic/fpezuul/FpeZuulApplication.java
@@ -0,0 +1,22 @@
+package fr.pereirafrederic.fpezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableZuulServer
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class FpeZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpeZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pereirafrederic/fpezuul/FpeZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pereirafrederic.fpezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpeZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
